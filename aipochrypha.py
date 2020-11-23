2bd37839af9603b5dd2731141c53f24457a574a8
cburdine <colinburdine443@gmail.com>
2020-11-23 08:28:34 -0600

    added trained model

--- aipochrypha.py
+++ aipochrypha.py
@@ -16,6 +16,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-f', '--file', metavar='file', help='file the model will be trained on')
 parser.add_argument('-n', '--name', metavar='name', help='the name of the model')
+parser.add_argument('-o', '--output', metavar='output', help='the output file')
 
 def load_file(file_path):
         
@@ -68,7 +69,8 @@ def network_loss(labels, logits):
 
 def generate_text(model, seed, 
         char2idx, idx2char, output_len=100,
-        entropy=1.0):
+        entropy=2.0,
+        filter_map=None):
 
     # vectorize seed string:
     input_eval = [ char2idx[s] for s in seed ]
@@ -84,12 +86,26 @@ def generate_text(model, seed,
         
         # add newly generated character to model:
         input_eval = tf.expand_dims([predicted_val], 0)
-        text_generated.append(idx2char[predicted_val])
-    
+        predicted_char = idx2char[predicted_val]
+        if filter_map is not None and predicted_char in filter_map.keys():
+            predicted_char = filter_map[predicted_char]
+        text_generated.append(predicted_char)
+
     # return result:
     return seed + ''.join(text_generated)
 
-
+def write_output_file(output_file, text, seed=None):
+    
+    with open(output_file, 'w') as fout:
+    
+        # write seed to header:
+        if seed is not None:
+            fout.write('SEED:\n')
+            fout.write(seed)
+            fout.write('\n')
+        
+        # dump text content:
+        fout.write(text)
 
 
 def main():
@@ -97,10 +113,12 @@ def main():
     args = parser.parse_args()
 
     file_in = args.file
+    file_out = args.output
     model_name = args.name
-    seq_len = 100
+    seq_len = 1000
     batch_size = 64
-    n_epochs = 10
+    n_epochs = 10000
+    output_len = 20000
      
     # load dataset:
     vocab, data, char2idx, idx2char = load_file(file_in)
@@ -121,20 +139,29 @@ def main():
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_prefix,
         save_weights_only=True)     
-
+    
     # train model:
-    history = network.fit(seq_dataset, epochs=n_epochs, callbacks=[checkpoint_callback])
+    #history = network.fit(seq_dataset, epochs=n_epochs, callbacks=[checkpoint_callback])
     
     # rebuild model with weights to take one input at a time:
     network = construct_network(len(vocab), 1)
     network.load_weights(tf.train.latest_checkpoint(checkpoint_dir)) 
     network.build(tf.TensorShape([1,None]))
+
+    # default filter map:
+    filter_map = { str(i) : '' for i in range(10) }
     
     text_seed = 'In those days there was no king in Israel: every man did that which was right in his own eyes.'
-
-    print(generate_text(network, seed=text_seed, 
-                        char2idx=char2idx, 
-                        idx2char=idx2char))
     
+    if file_out is not None:
+        text = generate_text(network, seed=text_seed, 
+                            char2idx=char2idx, 
+                            idx2char=idx2char,
+                            output_len=output_len,
+                            filter_map=filter_map,
+                            entropy=1.85)    
+        
+        write_output_file(file_out, text, seed=text_seed)
+
 if __name__ == '__main__':
     main()        
